e8c1b68efce36a6fecd0d107f6e25836133bef8f
Andres Rodriguez <andres.rodriguezp@usantotomas.edu.co>
2017-10-03 22:26:57 -0500

    presentacion

---
+++ Crear Presentacion.R
@@ -0,0 +1,8 @@
+##Presentación en Slidify
+##Instalar, la forma clasica da un error instala desde el repositorio de github
+library(devtools)
+install_github('slidify', 'ramnathv')
+install_github("slidifyLibraries", "ramnathv")
+
+library(slidify)
+author("presentacion")
